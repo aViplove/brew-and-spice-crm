b5b8dadad3e1d177978cff96141267f6ab75afa3
AdityaViplove <aviplove2@gmail.com>
2026-05-10 15:49:36 +0530

    removed ready function

--- core/apps.py
+++ core/apps.py
@@ -5,24 +5,3 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'Brew & Spice CRM'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        # Create admin user
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                password='admin123'
-            )
-
-        # Create staff user
-        if not User.objects.filter(username='barista').exists():
-            user = User.objects.create_user(
-                username='barista',
-                password='staff123'
-            )
-            user.is_staff = True
-            user.save()
